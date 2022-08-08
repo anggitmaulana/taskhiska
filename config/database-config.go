bdc7d345295a14299ff0d655ca29f255a78b6512
anggitmaulana <anggit.maulana@gmail.com>
2022-08-08 23:33:31 +0700

    add

---
+++ config/database-config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/ydhnwb/golang_api/entity"
+	"gorm.io/gorm"
+)
+
+//SetupDatabaseConnection is creating a new connection to our database
+func SetupDatabaseConnection() *gorm.DB {
+	//errEnv := godotenv.Load()
+	//if errEnv != nil {
+	//	panic("Failed to load env file")
+	//}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+	dbPort := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s port=%s dbname=%s sslmode=require TimeZone=Asia/Shanghai", dbHost, dbUser, dbPass, dbPort, dbName)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed to create a connection to database")
+	}
+	//nanti kita isi modelnya di sini
+	db.AutoMigrate(&entity.Book{}, &entity.User{})
+	return db
+}
+
+//CloseDatabaseConnection method is closing a connection between your app and your db
+func CloseDatabaseConnection(db *gorm.DB) {
+	dbSQL, err := db.DB()
+	if err != nil {
+		panic("Failed to close connection from database")
+	}
+	dbSQL.Close()
+}
--- go.mod
+++ go.mod
@@ -1,9 +1,17 @@
 module taskhiska
 
+// *heroku goVersion go1.18
 go 1.18
 
 require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/klauspost/compress v1.15.7 // indirect
 	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 	github.com/segmentio/kafka-go v0.4.33 // indirect
+	github.com/ydhnwb/golang_api v0.0.0-20210821145656-97a206998698 // indirect
+	gorm.io/driver/mysql v1.3.5 // indirect
+	gorm.io/gorm v1.23.8 // indirect
 )
